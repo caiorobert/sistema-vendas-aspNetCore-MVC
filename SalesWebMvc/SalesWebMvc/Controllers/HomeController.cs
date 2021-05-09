0c89ee04d70cec55605380d6119e8268d1f8371d
Caio Robert <caio.robert@live.com>
2021-05-09 15:32:15 -0300

    First controller and Razor pages tests

--- SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData["Student"] = "Caio Robert";
+            ViewData["Email"] = "caio.robert@live.com";
 
             return View();
         }
